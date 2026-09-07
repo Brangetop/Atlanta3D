8e3b9c66d546beb47855d04ab92007a394b40756
Brangetop <brangetop360@gmail.com>
2026-09-07 18:56:48 +0300

    rendering walls

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
 g++ main.c -o atlanta3D_engine -lGL -lGLU -lglut
+
+./atlanta3D_engine
--- main.c
+++ main.c
@@ -1,12 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
 
 float px,py; 
+
+int mapX=8,mapY=8,mapS=64;
+int map[]=
+{
+    1,1,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,1,
+    1,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,1,1,
+};
+
+void draw2DMap()
+{
+    int x,y,xo,yo;
+    for(y=0;y<mapY;y++) 
+    {
+        for(x=0;x<mapX;x++)
+        {
+            if(map[y * mapX + x] == 1)
+            {
+                glColor3f(1,1,1);
+            } else {
+                glColor3f(0,0,0);
+            }
+
+            xo=x*mapS;
+            yo=y*mapS;
+
+            glBegin(GL_QUADS);
+            glVertex2i(xo+1,yo+1);
+            glVertex2i(xo+1,yo+mapS-1);
+            glVertex2i(xo+mapS-1,yo+mapS-1);
+            glVertex2i(xo+mapS-1,yo+1);
+
+            glEnd();
+        }
+        
+    }
+}
 
 void drawPlayer() 
 {
@@ -21,14 +63,32 @@ void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
+    draw2DMap();
     drawPlayer();
     
     glutSwapBuffers();
 }
 
-void buttons(unsigned char key, int x. int y)
+void buttons(unsigned char key, int x, int y)
 {
-    
+    if(key=='w')
+    {
+        py-=5;
+    }
+    if(key=='s')
+    {
+        py+=5;
+    }
+    if(key=='a')
+    {
+        px-=5;
+    }
+    if(key=='d')
+    {
+        px+=5;
+    }
+
+    glutPostRedisplay();
 }
 void init()
 {
@@ -45,7 +105,12 @@ int main(int argc, char* argv[])
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowSize(1024,512);
     glutCreateWindow("Atlanta3D");
+
+    // wayland window size fix
+    //glutReshapeWindow(1024,512);
+
     init();
     glutDisplayFunc(display); 
+    glutKeyboardFunc(buttons);
     glutMainLoop();
 }
