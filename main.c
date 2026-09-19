ec3d044152ff7e34916251f51430b8b0f6d5c89c
Brangetop <brangetop360@gmail.com>
2026-09-19 23:55:55 +0300

    fixed segmentation faults

--- main.c
+++ main.c
@@ -38,13 +38,13 @@ int mapF[]=
 
 int mapC[]=
 {
-    0,2,2,2,2,1,1,0,
+    99,2,2,2,2,1,1,99,
     2,1,1,1,2,2,2,1,
     2,1,1,1,2,2,2,1,
     2,2,2,1,2,2,2,1,
-    1,0,0,0,0,0,0,3,
-    1,0,0,0,0,1,0,3,
-    1,0,0,0,0,0,0,1,
+    1,99,99,99,99,99,99,3,
+    1,99,99,99,99,1,99,3,
+    1,99,99,99,99,99,99,1,
     1,1,1,1,1,1,1,1,	
 };
 
@@ -234,15 +234,19 @@ void drawRays2D()
             int mp=mapF[(int)(ty/32.0)*mapX+(int)(tx/32.0)]*32*32;
             float c=All_Textures[((int)(ty)&31)*32 + ((int)(tx)&31)+mp]*0.7;
             glColor3f(c*0.7,c,c*0.7);
-            int tile_type=mapF[(int)(ty/32.0)*mapX+(int)(tx/32.0)]+1;
-            if(tile_type==2) { glColor3f(c,c*0.9,c*0.5); } // differet color for "planks" 
+            int tile_type=mapF[(int)(ty/32.0)*mapX+(int)(tx/32.0)];
+            // no roof logic
+            if(tile_type==99) { continue; }
+            if(tile_type==1) { glColor3f(c,c*0.9,c*0.5); } // differet color for "planks" 
             glPointSize(8);glBegin(GL_POINTS);glVertex2i(r*8+530,y);glEnd();
 
             // draw tha roof
             mp=mapC[(int)(ty/32.0)*mapX+(int)(tx/32.0)]*32*32;
+            tile_type=mapC[(int)(ty/32.0)*mapX+(int)(tx/32.0)];
+            if(tile_type==99) { continue; } // no roof
             c=All_Textures[((int)(ty)&31)*32 + ((int)(tx)&31)+mp]*0.4;
             glColor3f(c*0.7,c*0.7,c);
-            tile_type=mapC[(int)(ty/32.0)*mapX+(int)(tx/32.0)];
+            
             if(tile_type==1) { glColor3f(c,c*0.9,c*0.5); } // differet color for "planks" 
             glPointSize(8);glBegin(GL_POINTS);glVertex2i(r*8+530,320-y);glEnd();
         }
